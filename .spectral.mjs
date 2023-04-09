f1ae4a2dab936ba413354bb6f07d64cff6b6349f
Kegtron <info@kegtron.com>
2023-04-08 18:21:19 -0700

    Added new Kegtron.ManServUpdate endpoint

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE1NTc3/spectral.js?branch_slug=master&token=42077845-9173-4595-9d44-335ef7adfab3";
+export default { extends: ruleset };
